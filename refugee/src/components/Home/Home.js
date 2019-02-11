bad4c928084a9670b350b9733b12f20b04454f15
Imon Ovbude <imonovbude@gmail.com>
2019-02-11 14:46:06 -0800

    added carousel component to home page

--- refugee/src/components/Home/Home.js
+++ refugee/src/components/Home/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Nav from '../Nav/Nav'
+import CarouselSlides from './CarouselSlides';
 
 function Home() {
 
     return (
         <div>
             <Nav />
+            <CarouselSlides />
         </div>
     )
 }
