b0c219286e1aaa3eb181abb7339c63f3a237f6a0
Imon Ovbude <imonovbude@gmail.com>
2019-02-14 13:25:46 -0800

    finished styles for stories page

--- refugee/src/components/Stories/Story.js
+++ refugee/src/components/Stories/Story.js
@@ -6,9 +6,9 @@ const Story = (props) => {
     return (
         <div className='submit-story'>
             <div><img src={props.story.url_thumbnail} alt='thumbnail-pic' /></div>
-            <div><p>{props.story.title}</p></div>
-            <div><p>{props.story.snippet}</p></div>
-            <button><i className="fas fa-arrow-circle-right"></i>LEARN MORE</button>
+            <h3 className='container'>{props.story.title}</h3>
+            <div className='snippet-div container'><p>{props.story.snippet}</p></div>
+            <button className='container'><p><i className="fas fa-arrow-circle-right"></i></p><p>LEARN MORE</p></button>
         </div>
     )
 
